74817d53d7ae108d0ba80dfa2130ada3517d90fe
jelice <j.lice@hotmail.fr>
2023-05-31 16:43:46 +0200

    first commit

---
+++ models/connectionString.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const connectionString = process.env.MONGO_URL;
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+.then(() => console.log('Database connected'))
+
+.catch(error => console.error(error));
